bf8b21b3b08581795600800f8069c98f1bf5957e
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-08-15 20:23:25 +0530

    Updates

---
+++ precache-manifest.83828dd083c08a77931815f11ae913db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a88d1c6aa7f4afe4388707db0911c4",
+    "url": "/Tic-Tac-Toe-React/index.html"
+  },
+  {
+    "revision": "7b76a321a0f6bd1a73dd",
+    "url": "/Tic-Tac-Toe-React/static/css/main.8a935685.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/Tic-Tac-Toe-React/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe-React/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b76a321a0f6bd1a73dd",
+    "url": "/Tic-Tac-Toe-React/static/js/main.4dfa9504.chunk.js"
+  },
+  {
+    "revision": "0f645f25ca90b5d8bf77",
+    "url": "/Tic-Tac-Toe-React/static/js/runtime-main.e7258889.js"
+  }
+]);
